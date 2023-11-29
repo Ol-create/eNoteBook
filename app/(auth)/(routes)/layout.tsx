4ca063e5c60692f0479e09a17ad65e1a8feac97f
Ol-create <oluola96@gmail.com>
2023-11-29 17:44:05 +0100

    add heading component

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return <div>
+    return <div className='h-full bg-red-600'>
       {children}
   </div>;
 }
