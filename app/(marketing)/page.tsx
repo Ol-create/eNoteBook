2a49f76a400322925188bdfa92229a88b090ff9b
Ol-create <oluola96@gmail.com>
2023-11-30 16:15:53 +0100

    add Navbar

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Heading from './_components/Heading'
 import Hero from './_components/Hero'
+import Footer from './_components/Footer'
 
 export default function MarketingPage() {
   return (
@@ -8,7 +9,8 @@ export default function MarketingPage() {
           <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
         <Heading />
         <Hero />
-          </div>
+      </div>
+      <Footer />
     </div>
   )
 }
