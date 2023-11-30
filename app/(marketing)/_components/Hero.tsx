ea765a0fc4083cde9a06111d5914a32dd803e2b5
Ol-create <oluola96@gmail.com>
2023-11-30 14:53:32 +0100

    add second image to medium size device

--- app/(marketing)/_components/Hero.tsx
+++ app/(marketing)/_components/Hero.tsx
@@ -4,16 +4,23 @@ import Image from 'next/image'
 export default function 
 () {
   return (
-      <div className='flex items-center'>
-          <div className='relative w-[300px] h-[300px]  sm:w-[350px]'>
-              <Image
-                  src="/documents.png"
-                  fill
-                  className='object-contain'
-                  alt='Documents'
-              />
-          </div>
-
+    <div className="flex items-center">
+      <div className="relative w-[300px] h-[300px]  sm:w-[350px]">
+        <Image
+          src="/documents.png"
+          fill
+          className="object-contain"
+          alt="Documents"
+        />
+      </div>
+      <div className="relative w-[400px] h-[400px]  hidden md:block">
+        <Image
+          src="/reading.png"
+          fill
+          className="object-contain"
+          alt="Reading"
+        />
+      </div>
     </div>
-  )
+  );
 }
